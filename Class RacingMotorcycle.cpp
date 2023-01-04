7c86ef5ee2deeb6de564e16e2a08e1b9038fb777
Pavel Sadykov <2439747@gmail.com>
2023-01-05 02:05:04 +0500

    Create Class RacingMotorcycle.cpp

---
+++ Class RacingMotorcycle.cpp
@@ -0,0 +1,21 @@
+//
+//  Class RacingMotorcycle.cpp
+//  Races
+//
+//  Created by Павел on 04.01.2023.
+//
+
+#include "Class RacingMotorcycle.hpp"
+
+RacingMotorcycle:: RacingMotorcycle (){}
+
+int RacingMotorcycle:: getMotoSpeed(){
+    return _maxSpeed;
+}
+int RacingMotorcycle:: getMotoAcceleration(){
+    return _acceleration;
+}
+int RacingMotorcycle:: getMotoPatency(){
+    return _vehiclePatency;
+    
+}
